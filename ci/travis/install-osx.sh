3dadaf6c1446829a5aad5e62bd9fa2d48a7fd36b
Dmitry Kostrikin <dima.da222@yandex.ru>
2020-10-13 22:45:12 +0300

    commit over9000

--- ci/travis/install-osx.sh
+++ ci/travis/install-osx.sh
@@ -1,2 +1,6 @@
 brew update
 brew install ragel
+
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" < /dev/null 2> /dev/null
+brew install lcov
+brew install gcovr
