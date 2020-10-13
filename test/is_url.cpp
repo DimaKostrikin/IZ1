4734a8da306878a174a646aa8c25b94495f6c932
Dmitry Kostrikin <dima.da222@yandex.ru>
2020-10-14 00:37:02 +0300

    fdsksf

--- test/is_url.cpp
+++ test/is_url.cpp
@@ -17,3 +17,16 @@ TEST(is_url, ok) {
     ASSERT_EQ(is_url("https://vkcom"), false);
     ASSERT_EQ(is_url("https://vkcom."), false);
 }
+
+TEST(get_lexem, ok) {
+    char test1[10] = "123456://";
+    char test_dot[10] = "123456as.";
+    char test_space[10] = "123456as ";
+    char test_end[10] = "123456as\0";
+    char test_nothing[10] = "123456aaa";
+    ASSERT_EQ(get_lexem(test1), LEXEM_PROTOCOL);
+    ASSERT_EQ(get_lexem(test_dot), LEXEM_DOT);
+    ASSERT_EQ(get_lexem(test_space), LEXEM_SPACE);
+    ASSERT_EQ(get_lexem(test_end), LEXEM_END_OF_STRING);
+    ASSERT_EQ(get_lexem(test_nothing), LEXEM_NOTHING);
+}
