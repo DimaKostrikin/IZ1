1c7f78aaceb49f17f098a1654a3eb5ae7cf5d1bb
Dmitry Kostrikin <dima.da222@yandex.ru>
2020-10-13 23:48:14 +0300

    123232123

--- ci/travis/install-linux.sh
+++ ci/travis/install-linux.sh
@@ -3,7 +3,10 @@ sudo apt-get update -qq
 
 sudo apt-get install -qq g++-5
 sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-5 90
-sudo apg-get install gcc
+
+sudo apt-get install gcc
+sudo apt-get install gcovr
+sudo apt-get install -y lcov
 
 CMAKE_VERSION=3.3.2
 CMAKE_VERSION_DIR=v3.3
